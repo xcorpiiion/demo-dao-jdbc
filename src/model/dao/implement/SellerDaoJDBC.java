49e30e4ed8a6bc1b4235cf84a428d398706223e7
Vinicius Cruz <vinicius.da.silva.cruz.sanji@gmail.com>
2019-12-23 21:14:04 -0300

    Criacao das classes SellerDaoJDBC e da DaoFactory

---
+++ src/model/dao/implement/SellerDaoJDBC.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao.implement;
+
+import java.util.List;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+/**
+ *
+ * @author User
+ */
+public class SellerDaoJDBC implements SellerDao{
+
+    @Override
+    public void insert(Seller departament) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Seller department) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
